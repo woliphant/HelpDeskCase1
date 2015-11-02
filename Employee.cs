6ba22f32799fc98a31e0558608a65ad4f98070df
woliphant <will_oliphant@hotmail.com>
2015-11-02 13:01:20 -0500

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Kennedy;
+
+namespace HelpdeskDAL
+{
+    [System.Serializable]
+    public class Employee : IMongoEntity
+    {
+        public ObjectId _id { get; set; }
+        public string _accessId { get; set; }
+        public ObjectId DepartmentId { get; set; }
+        public string Title { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phoneno { get; set; }
+        public string Entity64 { get; set; }
+    }
+}
